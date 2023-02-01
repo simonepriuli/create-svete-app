49402332760f6f772bb0345733f9590e6ebc6f68
2dayevents <simone.priuli@icloud.com>
2023-02-01 18:52:26 +0100

    add precommit

---
+++ template/dist/middlewares/exampleMiddleware.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exampleMiddleware = void 0;
+function exampleMiddleware(req, res, next) {
+    const authHeader = req.headers["auth"];
+    if (authHeader === "john") {
+        next();
+    }
+    else {
+        res.status(400).send("Forbidden");
+    }
+}
+exports.exampleMiddleware = exampleMiddleware;
+//# sourceMappingURL=exampleMiddleware.js.map
