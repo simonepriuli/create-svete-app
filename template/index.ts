f359b8aae1a9e0967d588f4e441074404658e101
2dayevents <simone.priuli@icloud.com>
2023-02-01 18:07:36 +0100

    test

--- template/index.ts
+++ template/index.ts
@@ -8,7 +8,11 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use("/", exampleRouter);
+app.use("/exampleRouter", exampleRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).send("This is an example router!");
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
