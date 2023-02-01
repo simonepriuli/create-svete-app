2605c5fc52f0aec78bc5e9e45fd405b7f69905e2
2dayevents <simone.priuli@icloud.com>
2023-02-01 18:23:17 +0100

    test deploy

--- template/api/example.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response } from "express";
-
-export const exampleRouter = express.Router();
-exampleRouter.use(express.json());
-
-exampleRouter.get("/", (req, res) => {
-  res.status(200).send("This is an example router!");
-});
